44ae9bd75023da9276db3862659662e3ef5b6060
maxbdevelops <maxbdevelops@gmail.com>
2021-03-18 15:30:33 -0500

    Added alt property

--- src/Components/TrackSearchResult.js
+++ src/Components/TrackSearchResult.js
@@ -11,7 +11,7 @@ export default function TrackSearchResult({track, chooseTrack})
         className="d-flex m-2 align-items-center" 
         style={{cursor: 'pointer', backgroundColor: 'white', borderRadius: "5px"}}
         onClick={handlePlay}>
-            <img src={track.albumUrl} style={{height: '64px', width: '64px'}} />
+            <img src={track.albumUrl} alt="" style={{height: '64px', width: '64px'}} />
             <div className="ml-3 ">
                 <div>{track.title}</div>
                 <div className="text-muted" style={{fontWeight: "300"}}>{track.artist}</div>
