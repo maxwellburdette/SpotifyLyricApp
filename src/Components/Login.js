b13bc0fe57aa0e005c6781a286f3b48d496044d9
maxwellburdette <77358759+maxwellburdette@users.noreply.github.com>
2022-05-14 13:51:37 -0500

    Removes button to login. Instead automatically logs in

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container } from "react-bootstrap";
 const AUTH_URL = process.env.REACT_APP_AUTH_URL || process.env.AUTH_URL;
+
 export default function Login() {
+	useEffect(() => {
+		window.location.href = AUTH_URL;
+	}, []);
 	return (
 		<Container
 			className="d-flex justify-content-center align-items-center"
 			style={{ minHeight: "100vh" }}
 		>
-			<a className="btn btn-success btn-large" href={AUTH_URL}>
+			{/* <a className="btn btn-success btn-large" href={AUTH_URL}>
 				Login With Spotify
-			</a>
+			</a> */}
 		</Container>
 	);
 }
